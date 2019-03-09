af3855372249035a039d2e28884b16c50bb6735c
Peyton Tanzillo <peyton.tanzillo@huskers.unl.edu>
2019-03-08 19:39:29 -0600

    Cleaned up files so that I can properly start from scratch

---
+++ src/WebGraderApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './WebGraderApp.css';
+
+class WebGraderApp extends Component {
+  render() {
+    return (
+      <h1>Web Grader!</h1>
+    );
+  }
+}
+
+export default WebGraderApp;
--- src/WebGraderApp.test.js
+++ src/WebGraderApp.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import WebGraderApp from './WebGraderApp';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<WebGraderApp />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
